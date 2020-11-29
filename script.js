a017fbccf579cb82de0eef0f0838ff48371c1d40
Harkness Wolf <ammophila.procera@gmail.com>
2020-11-28 19:09:59 -0600

    Added response to mouse movement

--- script.js
+++ script.js
@@ -6,13 +6,18 @@ canvas.height = 500;
 let segmentsArray = [];
 const maxSegments = 10;
 
+let mouse = {
+  x: null,
+  y: null
+}
+
 class Segment {
   constructor(index, width) {
     this.index = index;
     this.width = width;
     this.ax = (this.index === 0) ? canvas.width/2 : segmentsArray[index - 1].bx;
     this.ay = (this.index === 0) ? canvas.height : segmentsArray[index - 1].by;
-    this.theta = -Math.PI / 2;
+    this.theta = 0;
     this.length = 30;
     this.dx = Math.cos(this.theta) * this.length;
     this.dy = Math.sin(this.theta) * this.length;
@@ -28,7 +33,23 @@ class Segment {
     ctx.lineTo(this.bx, this.by);
     ctx.stroke();
   }
-  update() {}
+  update() {
+    this.ax = (this.index === 0) ? canvas.width/2 : segmentsArray[this.index - 1].bx;
+    this.ay = (this.index === 0) ? canvas.height : segmentsArray[this.index - 1].by;
+    if (this.index !== 0) this.theta = segmentsArray[this.index - 1].theta - 0.5;
+    else if (this.index === 0) this.theta = calculateAngle(this.ax, this.ay);
+    this.dx = Math.cos(this.theta) * this.length;
+    this.dy = -Math.abs(Math.sin(this.theta) * this.length);
+    this.bx = this.ax + this.dx;
+    this.by = this.ay + this.dy;
+  }
+}
+
+function calculateAngle(xPos, yPos) {
+  let y = mouse.y - yPos;
+  let x = mouse.x - yPos;
+  let newAngle = Math.atan2(y, x);
+  return newAngle;
 }
 
 function animate() {
@@ -48,5 +69,11 @@ function init() {
   }
   animate();
 }
+
+window.addEventListener('mousemove', function(e) {
+  const boundary = canvas.getBoundingClientRect();
+  mouse.x = e.x - boundary.left;
+  mouse.y = e.y - boundary.top;
+});
 
 init();
